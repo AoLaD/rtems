2e5cbc61086765d6b95632ae6c5bc2c46aed95f0
Premysl Houdek <kom541000@gmail.com>
2014-07-23 18:42:46 +0200

    TMS570 IRQ support and Ticker test running.
    Signed-off-by: Premysl Houdek <kom541000@gmail.com>

--- c/src/lib/libbsp/arm/tms570/irq/irq.c
+++ c/src/lib/libbsp/arm/tms570/irq/irq.c
@@ -24,54 +24,16 @@
 #include <rtems/score/armv4.h>
 
 #include <bsp.h>
-#include <bsp/irq.h>
 #include <bsp/irq-generic.h>
-//#include <bsp/vim.h>
+#include <bsp/tms570-vim.h>
+#include <bsp/irq.h>
+#include <rtems/score/armv4.h>
 
-
-typedef struct{
-    int PARFLG; 	//InterruptVectorTableParityFlagRegister
-    int PARCTL; 	//InterruptVectorTableParityControlRegister
-    int ADDERR; 	//AddressParityErrorRegister
-    int FBPARERR; 	//Fall-BackAddressParityErrorRegister
-    int reserved1 [0x4/4];
-    int IRQINDEX; 	//IRQIndexOffsetVectorRegister
-    int FIQINDEX; 	//FIQIndexOffsetVectorRegister
-    int reserved2 [0x8/4];
-    int FIRQPR0; 	//FIQ/IRQProgramControlRegister0
-    int FIRQPR1; 	//FIQ/IRQProgramControlRegister1
-    int FIRQPR2; 	//FIQ/IRQProgramControlRegister2
-    int reserved3 [0x4/4];
-    int INTREQ0; 	//PendingInterruptReadLocationRegister0
-    int INTREQ1; 	//PendingInterruptReadLocationRegister1
-    int INTREQ2; 	//PendingInterruptReadLocationRegister2
-    int reserved4 [0x4/4];
-    int REQENASET0; 	//InterruptEnableSetRegister0
-    int REQENASET1; 	//InterruptEnableSetRegister1
-    int REQENASET2; 	//InterruptEnableSetRegister2
-    int reserved5 [0x4/4];
-    int REQENACLR0; 	//InterruptEnableClearRegister0
-    int REQENACLR1; 	//InterruptEnableClearRegister1
-    int REQENACLR2; 	//InterruptEnableClearRegister2
-    int reserved6 [0x4/4];
-    int WAKEENASET0; 	//Wake-upEnableSetRegister0
-    int WAKEENASET1; 	//Wake-upEnableSetRegister1
-    int WAKEENASET2; 	//Wake-upEnableSetRegister2
-    int reserved7 [0x4/4];
-    int WAKEENACLR0; 	//Wake-upEnableClearRegister0
-    int WAKEENACLR1; 	//Wake-upEnableClearRegister1
-    int WAKEENACLR2; 	//Wake-upEnableClearRegister2
-    int reserved8 [0x4/4];
-    int IRQVECREG; 	//IRQInterruptVectorRegister
-    int FIQVECREG; 	//FIQInterruptVectorRegister
-    int CAPEVT; 	//CaptureEventRegister
-    int CHANCTRL [0x5c/4]; //VIM Interrupt Control Register (PARSER ERROR)
-}VIM;
-VIM* vim = (VIM*)0xFFFFFDEC;
 
 static inline bool tms570_irq_is_valid(rtems_vector_number vector)
 {
-  return vector <= BSP_INTERRUPT_VECTOR_MAX && vector > BSP_INTERRUPT_VECTOR_MIN;
+  return (vector <= BSP_INTERRUPT_VECTOR_MAX) &&
+         (vector > BSP_INTERRUPT_VECTOR_MIN);
 }
 
 unsigned int priorityTable[BSP_INTERRUPT_VECTOR_MAX+1];//will be deleted
@@ -91,29 +53,19 @@ unsigned tms570_irq_get_priority(rtems_vector_number vector){
 
 void bsp_interrupt_dispatch(void)
 {
-	
-  rtems_vector_number vector = vim->IRQINDEX-1;
-     
+
+  rtems_vector_number vector = TMS570_VIM.IRQINDEX-1;
+
   bsp_interrupt_handler_dispatch(vector);
 }
 
 rtems_status_code bsp_interrupt_vector_enable(rtems_vector_number vector)
 {
-  unsigned int * enable_reg;
-  
   if(!tms570_irq_is_valid(vector))
-	 return RTEMS_INVALID_ID;  
-  
-  if(vector<32)
-	 enable_reg = vim->REQENASET0;
-  else if(vector<64 && vector >= 32)
-     enable_reg = vim->REQENASET1;
-  else if(vector<96 && vector >= 64)
-     enable_reg = vim->REQENASET2;
-  else
 	 return RTEMS_INVALID_ID;
-	 
-  *enable_reg |= (1<<(vector%32));    
+
+  TMS570_VIM.REQENASET[vector >> 5] = 1 << (vector & 0x1f);
+
   return RTEMS_SUCCESSFUL;
 }
 
@@ -121,41 +73,40 @@ rtems_status_code bsp_interrupt_vector_enable(rtems_vector_number vector)
 
 rtems_status_code bsp_interrupt_vector_disable(rtems_vector_number vector)
 {
-  unsigned int * disable_reg;
-  
   if(!tms570_irq_is_valid(vector))
-    return RTEMS_INVALID_ID; 
-  
-  if(vector<32)
-	 disable_reg = vim->REQENACLR0;
-  else if(vector<64 && vector >= 32)
-     disable_reg = vim->REQENACLR1;
-  else if(vector<96 && vector >= 64)
-     disable_reg = vim->REQENACLR2;
-  else
-	 return RTEMS_INVALID_ID;
-	 
-  *disable_reg |= (1<<(vector%32));    
+    return RTEMS_INVALID_ID;
+
+  TMS570_VIM.REQENACLR[vector >> 5] = 1 << (vector & 0x1f);
+
   return RTEMS_SUCCESSFUL;
 }
 
 rtems_status_code bsp_interrupt_facility_initialize(void)
 {
-	/*disable interrupts*/
-  vim->REQENACLR0 = 0xffffffff;
-  vim->REQENACLR1 = 0xffffffff;
-  vim->REQENACLR2 = 0xffffffff;
-    /*map default events on interrupt vectors*/
-  unsigned int value = 0x00010203,i=0;
-  for(i=0;i<24;i+=1,value += 0x01010101){
-	  vim->CHANCTRL[i] = value;
-  }
-    /*set all vectors as IRQ (not FIR)*/
-  vim->FIRQPR0 = 3;
-  vim->FIRQPR1 = 0;
-  vim->FIRQPR2 = 0;
+  void (**vim_vec)(void) = (void (**)(void)) 0xFFF82000;
+  unsigned int value = 0x00010203;
+  unsigned int i = 0;
+  uint32_t sctlr;
 
-  //_CPU_ISR_install_vector(ARM_EXCEPTION_IRQ, _ARMV4_Exception_interrupt, NULL);
+  /* disable interrupts */
+  for(i = 0; i < 3; i++)
+	TMS570_VIM.REQENACLR[i] = 0xffffffff;
+  /* map default events on interrupt vectors */
+  for(i=0;i<24;i+=1,value += 0x04040404)
+	TMS570_VIM.CHANCTRL[i] = value;
+  /* set all vectors as IRQ (not FIR) */
+  TMS570_VIM.FIRQPR[0] = 3;
+  TMS570_VIM.FIRQPR[1] = 0;
+  TMS570_VIM.FIRQPR[2] = 0;
+
+  /*_CPU_ISR_install_vector(ARM_EXCEPTION_IRQ, _ARMV4_Exception_interrupt, NULL);*/
+  for(i = 0; i <= 94; i++)
+	vim_vec[i] = _ARMV4_Exception_interrupt;
+
+  /* Clear bit VE in SCTLR register to not use VIM IRQ exception bypass*/
+  asm volatile ("mrc p15, 0, %0, c1, c0, 0\n": "=r" (sctlr));
+  sctlr &= ~(1 << 24);
+  asm volatile ("mcr p15, 0, %0, c1, c0, 0\n": : "r" (sctlr));
 
   return RTEMS_SUCCESSFUL;
 }
